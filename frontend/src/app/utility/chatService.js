997d0e502c1af791fe24529449eec0e00ebb5160
Victor <victoram03+git@gmail.com>
2025-06-29 10:55:17 -0400

    T4

--- frontend/src/app/utility/chatService.js
+++ frontend/src/app/utility/chatService.js
@@ -7,9 +7,9 @@ export function useWebSocket(username, onMessageReceived) {
     const clientRef = useRef(null);
     const API = process.env.NEXT_PUBLIC_BACKEND_API_URL;
     const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    const apiHost = API.replace(/^https?/, ''); // Remove protocol from API
+    const apiHost = API.replace(/^https?:/, ''); // Remove protocol from API
     const wsUrl = `${wsProtocol}:${apiHost}/ws`;
-    
+
     const connect = useCallback(() => {
         if (clientRef.current?.active) {
             console.log('WebSocket already connected');
